5e3f2d84c6ea32ddaca70642038d4f1e7710b7f2
nabcellent <nabcellent.dev@gmail.com>
2022-01-29 13:08:06 +0300

    Finished User & Product API

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -19,7 +19,8 @@ class ProductController extends ResourceController
     protected $format = 'json';
     private $product;
 
-    public function __construct() {
+    public function __construct()
+    {
         Services::eloquent();
 
         $this->product = new Product();
@@ -48,9 +49,10 @@ class ProductController extends ResourceController
      * Return the properties of a resource object
      *
      * @param null $id
-     * @return mixed
+     * @return Response
      */
-    public function show($id = null): mixed {
+    public function show($id = null): Response
+    {
         try {
             $product = $this->product->find($id);
 
